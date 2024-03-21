f5d8d901ff14eeb570c587d1b0ea6d3722866016
m0x0m0x <mx0mx@tuta.io>
2024-03-22 04:22:57 +0530

    123

--- W/S11/143.js
+++ W/S11/143.js
@@ -5,3 +5,16 @@
 import { mainHead } from "../../UTILS/text.js";
 // ===
 mainHead("143: Simple Arrays");
+
+/* 
+This section was extracted from 
+https://github.com/m0x0m0x/m-ujs-v1/blob/main/cursa/11-Arrays-Bankist/starter/script.js#L68-L74
+- You need to to back here for working on those files later 
+*/
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];

603b4f2031bfa82195df9ab1e627d2d4b789c395
m0x0m0x <mx0mx@tuta.io>
2024-03-24 00:11:02 +0530

    add

--- tezt/t1.js
+++ tezt/t1.js
@@ -1,6 +1,19 @@
 /* 
 t1:- Teztins
 */
-import { mainHead } from "../UTILS/text.js";
+import { mainHead, paraText } from "../UTILS/text.js";
 
 mainHead("Maps access test");
+
+// Array from 143.js
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+paraText("Print out the array and map");
+console.log(currencies);
+console.table(currencies);

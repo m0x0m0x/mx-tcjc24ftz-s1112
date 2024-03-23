a67da52a7555fb7f828f47dc93f8c3e1537f1f89
m0x0m0x <mx0mx@tuta.io>
2024-03-24 02:14:47 +0530

    144

--- W/S11/144.js
+++ W/S11/144.js
@@ -3,3 +3,24 @@
 */
 // === import zone
 import { mainHead, paraText, subHead } from "../../UTILS/text.js";
+mainHead("144: New At Method");
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+
+//using at method
+console.log(arr.at(0));
+
+paraText("Getting the last element");
+
+//getting last input of the array
+console.log(arr[arr.length - 1]);
+
+// using the slice method - get array with the method
+console.log(arr.slice(-1)[0]);
+
+paraText("Usign the at method - uses concepts of slice method");
+console.log(arr.at(-1));
+
+paraText("At method in strings");
+console.log("jam".at(-1));
